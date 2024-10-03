070c53827b6d1b3c3dac982fce15d71697bc0d7e
Hatem Soliman <hatemthedev@gmail.com>
2024-10-03 15:09:54 +0300

    Added exportable components

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
-// src/index.tsx
+import Navigation from "./Navigation";
+import Hero from "./Hero";
+import Features from "./Features";
+import CommandPrompt from "./CommandPrompt";
+import CopyCodeSnippet from "./CopyCodeSnippet";
+import Footer from "./Footer";
+
+export { Navigation, Hero, Features, CommandPrompt, CopyCodeSnippet, Footer };
