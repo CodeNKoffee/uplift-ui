d0767647952e7d2e466d26e54ffe75e3147729ce
Hatem Soliman <hatemthedev@gmail.com>
2024-10-03 12:51:30 +0300

    Added CommandPrompt component

---
+++ src/CommandPrompt.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+
+interface CommandProps {
+  command: string;
+}
+
+export default function CommandPrompt({ command }: CommandProps) {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(command);
+  };
+
+  return (
+    <div className="bg-gray-800 rounded-lg px-8 text-white text-lg flex justify-between items-center gap-8">
+      <pre className='py-4'>{command}</pre>
+      <button onClick={handleCopy}>
+        <FontAwesomeIcon icon={faCopy} />
+      </button>
+    </div>
+  );
+}
