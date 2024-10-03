85076ebbaab5b865bd9300ee31e46cda2bf99b7f
Hatem Soliman <hatemthedev@gmail.com>
2024-10-03 15:12:04 +0300

    Added declaration for exportable components

---
+++ src/declaration.d.ts
@@ -0,0 +1,29 @@
+declare module 'Navigation' {
+  const Navigation: React.ComponentType<any>;
+  export default Navigation;
+}
+
+declare module 'Hero' {
+  const Hero: React.ComponentType<any>;
+  export default Hero;
+}
+
+declare module 'Features' {
+  const Features: React.ComponentType<any>;
+  export default Features;
+}
+
+declare module 'CommandPrompt' {
+  const CommandPrompt: React.ComponentType<any>;
+  export default CommandPrompt;
+}
+
+declare module 'CopyCodeSnippet' {
+  const CopyCodeSnippet: React.ComponentType<any>;
+  export default CopyCodeSnippet;
+}
+
+declare module 'Footer' {
+  const Footer: React.ComponentType<any>;
+  export default Footer;
+}
