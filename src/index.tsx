85335edd41b06a5e9c3c080506588d3d76025138
Hatem Soliman <hatemthedev@gmail.com>
2024-10-04 10:18:21 +0300

    Code splitting made for components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,17 @@
-import Hero from "./Hero";
+import React, { lazy, Suspense } from "react";
 
-export { Hero };
+const Navigation = lazy(() => import("./esstentials/Navigation"));
+const Hero = lazy(() => import("./Hero"));
+const Features = lazy(() => import("./extras/Features"));
+const CommandPrompt = lazy(() => import("./code-blocks/CommandPrompt"));
+const CopyCodeSnippet = lazy(() => import("./code-blocks/CopyCodeSnippet"));
+const Footer = lazy(() => import("./esstentials/Footer"));
+
+export {
+  Navigation,
+  Hero,
+  Features,
+  CommandPrompt,
+  CopyCodeSnippet,
+  Footer,
+};
